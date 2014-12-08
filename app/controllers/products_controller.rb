b08df030556b1b1608f3c9129b5148d4d9532980
asell22 <alexanderjsell@gmail.com>
2014-12-08 10:39:41 -0800

    routes to show after create

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,35 @@
+class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render:new
+    end
+
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+    :name,
+    :description,
+    :price,
+    )
+  end
+end
